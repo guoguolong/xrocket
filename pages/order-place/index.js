3678793af8739f5c159d43fe828109cc2199128b
Allen Guo <elon.guo@gmail.com>
2021-09-08 12:47:57 +0800

    improve order place

--- pages/order-place/index.js
+++ pages/order-place/index.js
@@ -1,4 +1,25 @@
-function pageOrderPlace() {
-}
+function saveShippingAddr() {
+  $('.action-save-shipping-addr').onclick = function() {
+    var shippAddr = {};
+    shippAddr.firstName = $('.shipping-address-form input[name=firstName]').value;
+    shippAddr.lastName = $('.shipping-address-form input[name=lastName]').value;
+    shippAddr.address = $('.shipping-address-form input[name=address]').value;
+    shippAddr.apartment = $('.shipping-address-form input[name=apartment]').value;
+    shippAddr.city = $('.shipping-address-form input[name=city]').value;
+    shippAddr.country = $('.shipping-address-form select[name=country]').value;
+    shippAddr.state = $('.shipping-address-form select[name=state]').value;
+    shippAddr.zipcode = $('.shipping-address-form input[name=zipcode]').value;
+    shippAddr.phone = $('.shipping-address-form input[name=phone]').value;
 
+    $('.shipping-address-form').style.display = 'none';
+    $('.shipping-address-result').style.display = 'block';
+
+    console.log(shippAddr);
+  }
+}
+saveShippingAddr();
+
+function pageOrderPlace() {
+
+}
 pageOrderPlace();
