bc346f68810436535ed742f58dac30adaa973077
Allen Guo <elon.guo@gmail.com>
2021-09-12 01:26:32 +0800

    improve order place

--- pages/order-place/index.js
+++ pages/order-place/index.js
@@ -1,3 +1,7 @@
+function randomFrom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function setupStep() {
   if (window.location.hash === '#shipping' || !window.location.hash) {
     $('.shipping-wrapper').style.display = 'block';
@@ -14,17 +18,40 @@ function setupStep() {
   }  
 }
 
-setupStep();
-window.onhashchange = setupStep;
+var cart = JSON.parse(window.sessionStorage.getItem('cart'));
+if (cart) {
+  $('.order-place-page .main').style.display = 'initial';
+  $('.order-place-page .sidebar').style.display = 'initial';
+  $('.order-place-page .empty').style.display = 'none';
 
-if (typeof widgetOrderedProducts !== 'undefined') {
-  widgetOrderedProducts(JSON.parse(window.sessionStorage.getItem("cart")) || []);
-}
+  setupStep();
+  window.onhashchange = setupStep;
 
-$('.pay-now-wrapper .action-save').onclick = function() {
-  window.location.href = "/pages/order-created/index.html";
-}
+  if (typeof widgetOrderedProducts !== 'undefined') widgetOrderedProducts(cart);
+    
+  $('.pay-now-wrapper .action-save').onclick = function() {
+    var order = JSON.parse(window.sessionStorage.getItem('cart'));
+    order.id = `ON-${(new Date()).getTime()}`;
 
-$('.pay-now-wrapper .return-to-payment').onclick = function() {
-  window.location.href = "#payment";  
+    var orders = JSON.parse(window.localStorage.getItem('orders')) || {};
+    var user = JSON.parse(window.sessionStorage.getItem('user'));
+    orders[user.username] = orders[user.username] || [];
+    orders[user.username].push[order];
+
+    // 持久化到 localStorage
+    window.localStorage.setItem('orders', JSON.stringify(orders));
+
+    // 清空购物车
+    window.sessionStorage.removeItem("cart");
+
+    window.location.href = "/pages/order-created/index.html";
+  }
+
+  $('.pay-now-wrapper .return-to-payment').onclick = function() {
+    window.location.href = "#payment";  
+  }
+} else {
+  $('.order-place-page .main').style.display = 'none';
+  $('.order-place-page .sidebar').style.display = 'none';
+  $('.order-place-page .empty').style.display = 'initial';
 }
