7706b4c483cab7f2ffe8023937cbe0536600aff5
Allen Guo <elon.guo@gmail.com>
2021-09-18 00:09:31 +0800

    add image gallery

--- dist/widgets/image-gallery/index.js
+++ dist/widgets/image-gallery/index.js
@@ -1,31 +1,49 @@
-function widgetMedia(product, mediaWidth) {
+function widgetImageGallery(product, mainPicIndex, style) {
+  var pos = mainPicIndex;
   var images = product.images || [];
+  var MAX_COUNT = images.length;
+
   var imagesHtml = [];
   for (var i = 0; i < images.length; i++) {
-    imagesHtml.push(`
-<div class="small-pic-wrapper"><img data-url="${product.baseUrl}/${images[i]}" src="${product.baseUrl}/${images[i]}" /></div>
-      `)
+    imagesHtml.push(`<div class="image-wrapper"><img src="${product.baseUrl}/${images[i]}" /></div>`)
   }
-  $('.big-pic-wrapper').innerHTML = `<img src="${product.baseUrl}/${images[0]}" />`;
+  var $images = $('.image-gallery .images');
+  
+  $images.innerHTML = imagesHtml.join('');
 
-  if (images.length > 1) {
-    $('.small-pics').innerHTML = imagesHtml.join('');
+  if (MAX_COUNT <= 1) {
+    $('.next-btn').style.display = 'none';
+  } else {
+    $('.prev-btn').onclick = function() {
+      if (pos >= 0) { $('.prev-btn').style.display = 'none'; return;}
 
-    $('.small-pics').onclick = function(e) {
-      var imageUrl = e.target.dataset.url;
-      if (imageUrl) {
-        $('.big-pic-wrapper').innerHTML = `<img src="${imageUrl}" />`;
-      }
+      var width = $images.querySelector('.image-wrapper').offsetWidth;
+      
+      $('.next-btn').style.display = 'block';
+      pos++;
+      $('.image-gallery .images').style.left = pos * width + 'px';;
+    }
+    $('.next-btn').onclick = function() {
+      if (pos <= -(MAX_COUNT - 1)) { $('.next-btn').style.display = 'none'; return; }
+      var width = $images.querySelector('.image-wrapper').offsetWidth;
+      
+      $('.prev-btn').style.display = 'block';
+      pos--;
+      console.log('next pos::', pos)
+      $('.image-gallery .images').style.left = pos * width + 'px';
     }
   }
-  if (mediaWidth) {
-    $('.media').style.width = mediaWidth;
-  }
+
+  applyStyle(style, '.image-gallery');
 }
 
 if (window.location.pathname.match(/^\/widgets/)) {
-  widgetMedia({
+  widgetImageGallery({
     images:['1.png','2.png','3.png','4.png','5.png','6.png'],
     baseUrl: '/data/catalog/mens/t-shirts/f9-t-shirt-black-2x'
-  }, 600);
+  }, 0, {
+    padding: 0,
+    margin: '30px auto',
+    width: '500px',    
+  });
 }
